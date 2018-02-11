e0b022774a7174be0f8a2b7bf5b596e5ae7b63af
Landon Wark <lando.wark@gmail.com>
2018-02-11 13:34:47 -0600

    Added in new dialog for adding samples.

---
+++ UI/addSample.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'addSample.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QDialog
+
+class Ui_Dialog(QDialog):
+
+    def setupUi(self, Dialog):
+
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(678, 560)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../UI/icons/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+
+        print(type(self))
+        self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(13, 19, 651, 520))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+
+        self.Sample_Number = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.Sample_Number.setObjectName("Sample Number")
+
+        self.gridLayout.addWidget(self.Sample_Number, 1, 0, 1, 1)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.buttonBox.accepted.connect(self.Accept)
+        self.buttonBox.rejected.connect(self.Reject)
+        self.gridLayout.addWidget(self.buttonBox, 3, 0, 1, 1)
+
+        self.Patient_Number = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.Patient_Number.setText("")
+        self.Patient_Number.setClearButtonEnabled(False)
+        self.Patient_Number.setObjectName("Patient Number")
+
+        self.gridLayout.addWidget(self.Patient_Number, 0, 0, 1, 1)
+
+        self.calendarWidget = QtWidgets.QCalendarWidget(self.gridLayoutWidget)
+        self.calendarWidget.setVerticalHeaderFormat(QtWidgets.QCalendarWidget.NoVerticalHeader)
+        self.calendarWidget.setObjectName("calendarWidget")
+        self.gridLayout.addWidget(self.calendarWidget, 2, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Add Sample"))
+        self.Sample_Number.setPlaceholderText(_translate("Dialog", "Sample Number"))
+        self.Patient_Number.setPlaceholderText(_translate("Dialog", "Patient Number"))
+
+    def Accept(self):
+        patNum = self.Patient_Number.text()
+        samNum = self.Sample_Number.text()
+        dateRec = self.calendarWidget.selectedDate().toPyDate()
+        Dialog.close()
+
+    def Reject(self):
+        Dialog.close()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
--- UI/mainUI.py
+++ UI/mainUI.py
@@ -8,11 +8,15 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QGraphicsView, QMenuBar, QPushButton, QStatusBar, QGridLayout
+from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QGraphicsView, QMenuBar, QPushButton, QStatusBar, QGridLayout, QMainWindow
 import pymongo as mng
 from MongoInterface import mongo
 from ScrapeTeloView import telomgraph_emulator as te
 import os
+from PyQt5.QtGui import QIcon
+from UI import addSample
+
+# icon_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'icons', "logo.png")
 
 class Ui_MainWindow(object):
 
@@ -23,6 +27,11 @@ class Ui_MainWindow(object):
         MainWindow.resize(1064, 606)
         # set layout to grid
         grid = QGridLayout()
+
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../UI/icons/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+
         # config central widget
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -32,7 +41,7 @@ class Ui_MainWindow(object):
         self.treeWidget = QTreeWidget(self.centralwidget)
         self.treeWidget.setGeometry(QtCore.QRect(10, 10, 291, 511))
         self.treeWidget.setObjectName("treeWidget")
-        # View of Matplotlib... maybe
+        # View of Matplotlib... when I get it working
         self.graphicsView = QGraphicsView(self.centralwidget)
         self.graphicsView.setGeometry(QtCore.QRect(330, 10, 711, 281))
         self.graphicsView.setObjectName("graphicsView")
@@ -44,14 +53,24 @@ class Ui_MainWindow(object):
 
         # Main Window config stuff
         MainWindow.setCentralWidget(self.centralwidget)
+        # config menubar.
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1064, 38))
         self.menubar.setObjectName("menubar")
+        self.menuSamples = QtWidgets.QMenu(self.menubar)
+        self.menuSamples.setObjectName("menuSamples")
+        self.actionAdd_Sample = QtWidgets.QAction(MainWindow)
+        self.actionAdd_Sample.setObjectName("actionAdd_Sample")
+        self.menuSamples.addAction(self.actionAdd_Sample)
+        self.menubar.addAction(self.menuSamples.menuAction())
+
         MainWindow.setMenuBar(self.menubar)
+
+        # Config statusbar
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
+        # Add widgets to grid
         grid.addWidget(self.treeWidget, 0, 1)
         grid.addWidget(self.graphicsView, 0, 2)
         grid.addWidget(self.teloButton, 1, 2)
@@ -64,6 +83,7 @@ class Ui_MainWindow(object):
 
         # Connect buttons
         self.teloButton.clicked.connect(self.teloButtonClicked)
+        self.actionAdd_Sample.triggered.connect(self.showDialog)
 
 
     def retranslateUi(self, MainWindow):
@@ -73,6 +93,8 @@ class Ui_MainWindow(object):
         self.treeWidget.headerItem().setText(0, _translate("MainWindow", "Patients"))
         self.teloButton.setToolTip(_translate("MainWindow", "Generate Telomgraphs for selected filters."))
         self.teloButton.setText(_translate("MainWindow", "Generate"))
+        self.menuSamples.setTitle(_translate("MainWindow", "Samples"))
+        self.actionAdd_Sample.setText(_translate("MainWindow", "Add Sample"))
 
     def createDataTree(self):
         db = mng.MongoClient().prostate_actual.patient
@@ -90,29 +112,37 @@ class Ui_MainWindow(object):
     def teloButtonClicked(self):
         self.statusbar.showMessage("Telo Button Clicked!")
         root = self.treeWidget.invisibleRootItem()
+        # Count number of patients
         patient_count = root.childCount()
         samples_list = []
-        checked = {}
+        # iterate through patients
         for iii in range(patient_count):
             patient = root.child(iii)
-            #checked_sweeps = []
+            # count number of filters per patient
             filter_count = patient.childCount()
+            # iterate through filters
             for jjj in range(filter_count):
                 filter = patient.child(jjj)
+                # check filter item status checked/unchecked
                 if filter.checkState(0) == Qt.Checked:
-                    #checked_sweeps.append(filter.text(0).split(" ")[0])
+                    # add checked item to sample list for telomgraph
                     samples_list.append((patient.text(0), filter.text(0).split(" ")[0]))
-            #checked[patient.text(0)] = checked_sweeps
         for sample in samples_list:
             dicto = mongo.get_filter_by_number(sample[0], sample[1])
             timePoint = dicto['tPoint']
             sample_title = sample[0] + " " + timePoint + " " + sample[1] + ".xlsx"
             te.telomgraph(dicto, os.path.join("C:\\Users\\Landon\\Desktop", sample_title))
 
+    def showDialog(self):
+        self.sampleDialog = QtWidgets.QDialog()
+        self.ui = addSample.Ui_Dialog()
+        self.ui.setupUi(self.sampleDialog)
+        self.sampleDialog.exec_()
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
+    # app.setWindowIcon(QIcon(icon_location))
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
